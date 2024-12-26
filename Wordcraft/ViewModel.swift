bb5802fe96ff4e1bff73d6d9e4b0fbc7e78d723b
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-12-26 06:51:10 +0100

    Add select method

--- Wordcraft/ViewModel.swift
+++ Wordcraft/ViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 @Observable
 class ViewModel {
     var columns = [[Tile]]()
+    private var selected = [Tile]()
 
     init() {
         for i in 0..<5 {
@@ -21,6 +22,20 @@ class ViewModel {
             }
 
             columns.append(column)
+        }
+    }
+
+    func select(_ tile: Tile) {
+        if selected.last == tile && selected.count > 3 {
+            // check their word
+        } else if let index = selected.firstIndex(of: tile) {
+            if selected.count == 1 {
+                selected.removeLast()
+            } else {
+                selected.removeLast(selected.count - index - 1)
+            }
+        } else {
+            selected.append(tile)
         }
     }
 }
--- Wordcraft/WordcraftApp.swift
+++ Wordcraft/WordcraftApp.swift
@@ -13,5 +13,6 @@ struct WordcraftApp: App {
         WindowGroup {
             ContentView()
         }
+        .windowResizability(.contentSize)
     }
 }
