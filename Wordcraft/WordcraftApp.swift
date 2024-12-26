12fa162e0a1a642befba3d20b4a77e72a6b33c58
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2024-12-26 06:22:05 +0100

    Initial Commit

---
+++ Wordcraft/WordcraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordcraftApp.swift
+//  Wordcraft
+//
+//  Created by Eric on 26/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordcraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
